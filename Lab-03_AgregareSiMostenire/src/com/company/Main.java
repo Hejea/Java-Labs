7d187c717b2f7a3deda1597fc437dad3b0f0c21e
Hejea <hejea@mail.ru>
2018-06-01 14:58:10 +0300

    was added QA, QI, Test

---
+++ Lab-03_AgregareSiMostenire/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	
+        
+        // Inheristance
+        QueueInheritance.go();
+        System.out.println();
+        
+        // Aggregation
+        QueueAggregation.go();
+        System.out.println();
+        // Test
+        Test.go();
+        System.out.println();
+    }
+}
